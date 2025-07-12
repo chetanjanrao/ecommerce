347c1d5d1338ca3dcb58cd9ac6d66d7466a6dd99
Chetan Janrao <jrchetan1997@gmail.com>
2025-07-12 18:18:39 +0530

    producton ready code:working

--- app/(store)/basket/page.tsx
+++ app/(store)/basket/page.tsx
@@ -52,7 +52,7 @@ function BasketPage() {
          <div className="flex items-center justify-center min-h-screen bg-gray-100">
             <div className="text-center p-8 bg-white rounded-2xl shadow-lg">
                <h1 className="text-3xl font-bold text-gray-800 mb-4">
-                Your 🛒  Basket is Empty
+                Your Basket 🛒 is Empty
                </h1>
             </div>
          </div>
