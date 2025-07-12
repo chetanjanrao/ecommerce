c5731fdbed0b20ce949d6504bc4593fd3814a8dc
Chetan Janrao <jrchetan1997@gmail.com>
2025-07-12 14:50:58 +0530

    previous error resoved

--- app/(store)/search/page.tsx
+++ app/(store)/search/page.tsx
@@ -8,36 +8,32 @@ type SearchPageProps = {
 };
 
 async function SearchPage({ searchParams }: SearchPageProps) {
+  const query = searchParams.query || "";
+  const products = await searchProductsByName(query);
 
-      const params =await searchParams;
-      const query = params.query || "";
-      // console.log("searchParams", query);
-      const products = await searchProductsByName(query);
-      
-      if (!products.length) {
-            return (
-                  <div className="flex  flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
-                        <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
-                              <h1 className="text-3xl font-bold mb-6 text-center">
-                                    No products found for : {query}
-                              </h1>
-                              <p className="text-gray-600 text-center">
-                                    try searching with different keywords
-                              </p>
-                        </div>
-                  </div>
-            )
-      }
-      return (
-            <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
-                  <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
-                        <h1 className="text-3xl font-bold mb-6 text-center">
-                              Search results for : {query}
-                        </h1>
-                        <ProductGrid products={products} />
-                  </div>
-            </div>
-      )
+  return (
+    <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
+        {products.length > 0 ? (
+          <>
+            <h1 className="text-3xl font-bold mb-6 text-center">
+              Search results for: {query}
+            </h1>
+            <ProductGrid products={products} />
+          </>
+        ) : (
+          <>
+            <h1 className="text-3xl font-bold mb-6 text-center">
+              No products found for: {query}
+            </h1>
+            <p className="text-gray-600 text-center">
+              Try searching with different keywords.
+            </p>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
 
